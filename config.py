52eee864cc064875685215a0ce428325fc15d859
Carl Saginsin <carlsaginsin@Carls-MacBook-Pro.local>
2024-09-04 11:15:02 +0800

    Finished the MVP version of the TelegramBot + A.I

--- config.py
+++ config.py
@@ -9,15 +9,14 @@ TOKEN = os.getenv('TELEGRAM_BOT_API')
 TIMEZONE = os.getenv('TIMEZONE')
 TIMEZONE_COMMON_NAME = os.getenv('TIMEZONE_COMMON_NAME')
 BOT_USERNAME = os.getenv('TELEGRAM_BOT_USERNAME')
-api_key= os.getenv('api_key')
+IMG_OCR_API_KEY  = os.getenv('IMG_OCR_API_KEY')  # Corrected line
 
 # Use the variables
 print(f"Token: {TOKEN}")
 print(f"Timezone: {TIMEZONE}")
 print(f"Timezone Common Name: {TIMEZONE_COMMON_NAME}")
 print(f"Bot Username: {BOT_USERNAME}")
-print(f"IMGOCR API: {api_key}")
-
+print(f"IMGOCR API Key: {IMG_OCR_API_KEY}")
 
 # Get the additional variables
 MONDAY_API_TOKEN = os.getenv('MONDAY_API_TOKEN')
@@ -30,5 +29,4 @@ print(f"Monday API Token: {MONDAY_API_TOKEN}")
 print(f"Policy Board ID: {POLICY_BOARD_ID}")
 print(f"Referrer Board ID: {REFERRER_BOARD_ID}")
 print(f"Insurance Board ID: {INSURANCE_BOARD_ID}")
-
-
+print(f"IMGOCR API: {IMG_OCR_API_KEY}")
